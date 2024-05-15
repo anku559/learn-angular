61bbb78ac665bf25454bfae61b41d239dc3b13db
anku559 <ankitsingh91@gmail.com>
2024-05-15 12:40:21 +0530

    Completed

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Learning
 
-- [Password Generator](https://github.com/anku559/learn-angular/tree/ln/app-1.0-pwd-gen?tab=readme-ov-file)
+- Typing Project
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { el, faker } from '@faker-js/faker';
+
+const IS_MATCHED_TEXT = 'mt-3 has-text-centered';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'FE';
+  randomText: string = '';
+  enteredText: string = '';
+
+  constructor() {
+    this.randomText = faker.lorem.words(10);
+  }
+
+  @HostListener('document:copy', ['$event'])
+  disableCopy(event: ClipboardEvent) {
+    event.preventDefault();
+  }
+
+  onTextInput(e: Event) {
+    const textData = e.target as HTMLInputElement;
+    this.enteredText = textData.value;
+  }
+
+  onKeyDown(event: KeyboardEvent): void {
+    // Check if the key pressed is the backspace key
+    // https://developer.mozilla.org/en-US/docs/Web/API/UI_Events/Keyboard_event_key_values
+    if (event.key === 'Backspace') {
+      event.preventDefault(); // Prevent the default backspace behavior
+    }
+  }
+
+  compare(randomChar: string, inputChar: string) {
+    if (!inputChar) {
+      return 'pending';
+    }
+
+    if (randomChar === inputChar) {
+      return 'correct';
+    } else {
+      return 'incorrect';
+    }
+  }
 }
